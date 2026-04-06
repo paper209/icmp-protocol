4b27dcb04c3d9275ecf3e5b562a7980e5c2f1411
paper209 <fleshed-stroll84@icloud.com>
2026-04-06 10:45:02 +0900

    Remove unused ICMP echo functions and methods
    Removed the ReadEcho and ReadEchoIdentifier functions along with the SendEcho method from the Echo struct, cleaning up unused code.

--- icmp/icmp.go
+++ icmp/icmp.go
@@ -2,8 +2,6 @@ package icmp
 
 import (
 	"fmt"
-	"study/ip"
-	"study/util"
 	"syscall"
 )
 
@@ -14,87 +12,4 @@ func Read(s int) ([]byte, error) {
 		return nil, fmt.Errorf("socket error: %w", err)
 	}
 	return buf[:n], nil
-}
-
-func ReadEcho(s int) (*ip.Header, *Echo, error) {
-	for {
-		buf, err := Read(s)
-		if err != nil {
-			return nil, nil, fmt.Errorf("read error: %w", err)
-		}
-
-		ih, err := ip.DecodeHeader(buf)
-		if err != nil {
-			return nil, nil, fmt.Errorf("decode ip header error: %w", err)
-		}
-
-		h, err := decodeHeader(buf)
-		if err != nil {
-			return nil, nil, fmt.Errorf("decode icmp header error: %w", err)
-		} else if h.Type != 8 {
-			continue
-		}
-
-		e, err := decodeEcho(buf)
-		if err != nil {
-			return nil, nil, fmt.Errorf("decode echo error: %w", err)
-		}
-
-		return ih, e, nil
-	}
-}
-
-func ReadEchoIdentifier(s int, identifier uint16) (*Echo, error) {
-	for {
-		_, e, err := ReadEcho(s)
-		if err != nil {
-			return nil, fmt.Errorf("raed echo error: %w", err)
-		}
-
-		if e.Identifier == identifier {
-			return e, nil
-		}
-	}
-}
-
-func (e *Echo) SendEcho(address [4]byte) error {
-	src, err := util.SourceIP()
-	if err != nil {
-		return fmt.Errorf("get source ip error: %w", err)
-	}
-
-	ih := &ip.Header{
-		VersionIHL:         (4 << 4) | (5 & 0x0F),
-		Tos:                0,
-		TotalLength:        uint16(20 + 8 + len(e.Data)),
-		Identification:     0,
-		FlagsFragment:      0x4000, // df = 1
-		TTL:                64,
-		Protocol:           1, // icmp
-		SourceAddress:      src,
-		DestinationAddress: address,
-	}
-	buf := ih.BuildHeader()
-	buf = append(buf, e.buildEchoRequest()...)
-
-	s, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_RAW)
-	if err != nil {
-		return fmt.Errorf("socket errror: %s", err.Error())
-	}
-	defer syscall.Close(s)
-
-	err = syscall.SetsockoptInt(s, syscall.IPPROTO_IP, syscall.IP_HDRINCL, 1)
-	if err != nil {
-		return fmt.Errorf("socket option error: %s", err.Error())
-	}
-
-	err = syscall.Sendto(s, buf, 0, &syscall.SockaddrInet4{
-		Port: 0,
-		Addr: address,
-	})
-	if err != nil {
-		return fmt.Errorf("socket send error: %s", err.Error())
-	}
-
-	return nil
 }

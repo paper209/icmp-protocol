7d7d2a4ee1ab173be2d84b2c418520f43b395f7f
paper209 <fleshed-stroll84@icloud.com>
2026-04-07 13:54:36 +0900

    Add Write and Read functions in main.go
    Implement Write and Read functions for protocol communication.

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"study/protocol"
+	"time"
+)
+
+// src = 10.0.0.1
+func Write() {
+	c, err := protocol.NewConnection([4]byte{10, 0, 0, 2})
+	if err != nil {
+		panic(err)
+	}
+
+	err = c.Write([]byte("hello"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+// src = 10.0.0.2
+func Read() {
+	c, err := protocol.NewConnection([4]byte{10, 0, 0, 1})
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, 150)
+	for {
+		n, err := c.Read(buf)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(buf[:n])
+		time.Sleep(1000 * time.Millisecond)
+	}
+}
+
+func main() {
+	Read()
+}
